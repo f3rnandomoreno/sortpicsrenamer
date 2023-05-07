6600c61d909c0780ca8e586c376b6da0350c3514
Fernando Moreno Ruiz <fernandomorenoruiz@gmail.com>
2023-05-07 21:31:14 +0200

    remove unused comment

--- src/main/java/org/moreno/sortpics/gui/DuplicatesWindow.java
+++ src/main/java/org/moreno/sortpics/gui/DuplicatesWindow.java
@@ -138,9 +138,7 @@ public class DuplicatesWindow extends JFrame {
 
                     var numberOfUnselectedRowsWithId = numberOfUnselectedRowWithId(id);
                     if (folder.equals(selectedFolder) && numberOfUnselectedRowsWithId > 1) {
-                        //if (!id.equals(selectedId) || (id.equals(selectedId) && numberOfUnselectedRowsWithId > 1)) {
                         tableModel.setValueAt(true, i, 0);
-                        //}
                     }
                 }
             }
